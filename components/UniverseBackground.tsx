4b127d7e264b3426dac5638feaad6cf4f50e8f26
Ronaldop333 <35382410+Ronaldop333@users.noreply.github.com>
2025-10-18 01:08:12 -0300

    Update UniverseBackground.tsx

--- components/UniverseBackground.tsx
+++ components/UniverseBackground.tsx
@@ -7,18 +7,10 @@ import * as random from 'maath/random/dist/maath-random.cjs'
 export function UniverseBackground(props: any) {
   const ref = useRef<any>()
   const count = 8000
-  const speed = 0.05
 
-  const [spherePositions, starSizes] = useMemo(() => {
-    const positions = random.inSphere(new Float32Array(count * 3), { radius: 1.5 })
-    const sizes = new Float32Array(count)
-    
-    for (let i = 0; i < count; i++) {
-      sizes[i] = Math.random() * 0.01 + 0.001 // Tamanhos variados
-    }
-    
-    return [positions, sizes]
-  }, [count])
+  const sphere = useMemo(() => 
+    random.inSphere(new Float32Array(count * 3), { radius: 1.5 })
+  , [count])
 
   useFrame((state, delta) => {
     if (ref.current) {
@@ -29,14 +21,13 @@ export function UniverseBackground(props: any) {
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={spherePositions} stride={3} frustumCulled={false}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false}>
         <PointMaterial
           transparent
           vertexColors
           size={0.002}
           sizeAttenuation={true}
           depthWrite={false}
-          blending={1} // NormalBlending
         />
       </Points>
     </group>
