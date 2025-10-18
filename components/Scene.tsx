188c18200e77c74055552b7145ae79740a3f23af
Ronaldop333 <35382410+Ronaldop333@users.noreply.github.com>
2025-10-18 00:55:08 -0300

    Create Scene.tsx

---
+++ components/Scene.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { Canvas } from '@react-three/fiber'
+import { UniverseBackground } from './UniverseBackground'
+
+export function Scene() {
+  return (
+    <div className="fixed inset-0 -z-10">
+      <Canvas
+        camera={{ position: [0, 0, 1] }}
+        gl={{ alpha: true, antialias: false }}
+        dpr={[1, 2]} // Adaptive pixel ratio
+      >
+        <color attach="background" args={['#0f172a']} />
+        <UniverseBackground />
+      </Canvas>
+    </div>
+  )
+}
